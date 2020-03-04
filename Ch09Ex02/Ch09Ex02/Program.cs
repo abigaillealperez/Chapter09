7ce6fc990630777578f39b4e57e3485706a7cc4e
abigaillealperez <lealperezabigail.alp@gmail.com>
2020-03-04 03:13:52 -0500

    Create Program.cs

---
+++ Ch09Ex02/Ch09Ex02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ch09ClassLibrary;
+
+namespace Ch09Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyExternalClass myObj = new MyExternalClass();
+            Console.WriteLine(myObj.ToString());
+            Console.ReadKey();
+        }
+    }
+}
